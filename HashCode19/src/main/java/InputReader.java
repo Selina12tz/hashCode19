797b1e6454950ca869f70afe3998048d29a6ad0e
Sotiria Tzifa <stzifa@tradingpoint.com>
2019-02-28 18:35:43 +0200

    First commit

---
+++ HashCode19/src/main/java/InputReader.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class InputReader {
+
+    private String fileName;
+
+    public InputReader(String fileName) {
+        this.fileName = "input_files/" + fileName + ".in";
+    }
+
+    public ArrayList<int[]> readFile() {
+        FileReader fr;
+        BufferedReader br;
+
+        String fileLine;
+        String[] fileArray;
+
+        ArrayList<int[]> file = new ArrayList<>();
+        int[] outputArray;
+
+        try {
+            fr = new FileReader(fileName);
+            br = new BufferedReader(fr);
+
+            while ((fileLine = br.readLine()) != null) {
+                outputArray = new int[6];
+                fileArray = fileLine.split(" ");
+
+
+                for(int i = 0; i < fileArray.length; i++) {
+                    outputArray[i] = Integer.parseInt(fileArray[i]);
+                    System.out.println(outputArray[i]);
+                }
+
+
+                file.add(outputArray);
+            }
+
+
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found.");
+        } catch (IOException e) {
+            System.out.println("I/O Exception");
+        }
+
+        return file;
+    }
+
+}
---
+++ HashCode19/src/main/java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+
+    public static void main(String[] args){
+
+        InputReader inputReader = new InputReader("a_example");
+        inputReader.readFile();
+
+    }
+}
