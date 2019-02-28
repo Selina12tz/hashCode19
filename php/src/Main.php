374a3ff815378e99a69542d83d92352b9ca07e5a
Filippos Karailanidis <phil.230@gmail.com>
2019-02-28 22:50:49 +0200

    PHP code

--- php/src/Main.php
+++ php/src/Main.php
@@ -5,7 +5,7 @@ namespace App;
 
 function _($a): void
 {
-    if (true) {
+    if (false) {
         if (is_array($a)) {
             foreach ($a as $b) {
                 echo $b  . "\n";
@@ -20,77 +20,127 @@ class Main
     public function __invoke(string $filename)
     {
         $input = new Input($filename);
+        $photos = $input->parsePhotos();
         $output = new Output();
 
-        $r1 = 0;
-        $r2 = $r1 + 1;
-        $lastRow = new Row();
-
-        while ($r2 <= $input->getR()) {
-            $row = $this->sliceRow($r1, $r2, $input);
-            if ($row->getScore() < $lastRow->getScore()) {
-                _("adding row");
-                $output->addRow($row);
-
-                $r1 = $r2;
-                $r2 = $r1 + 1;
-                $lastRow = new Row();
+        //stage 1: form slides
+        //TODO score
+        $allSlides = [];
+        $slideId = 0;
+        $verticals = [];
+        foreach ($photos as $photo) {
+            if ($photo->getOrientation() == 'H') {
+                $slide = new Slide($slideId);
+                $slide->addPhoto($photo);
+                $allSlides[$slideId] = $slide;
+                $slideId++;
             } else {
-                $r2++;
-                $lastRow = $row;
+                $verticals[] = $photo;
             }
         }
-        if ($lastRow->getScore() != 0) {
-            _("adding row");
-            $output->addRow($lastRow);
+        $verticalSlides = $this->populateVerticals($verticals, $slideId);
+        // keep keys
+        $allSlides = $allSlides + $verticalSlides;
+
+        //stage 2: create tag dictionary
+        $tags = [];
+        foreach ($allSlides as $slide) {
+            foreach ($slide->getTags() as $tag) {
+                if (!isset($tags[$tag])) {
+                    $tags[$tag] = [];
+                }
+                $tags[$tag][] = $slide->getId();
+            }
         }
 
+        //stage 3: create output based on tags
+        //TODO score, second run, in case more than 2
+        foreach ($tags as $tag => $slideIds) {
+            // TODO score Get ANY tag
+            if (count($tags[$tag]) >= 2) {
+                _("Using Tag $tag");
+                //TODO score pick slideIds
+                $slide1 = $allSlides[$tags[$tag][0]];
+                $slide2 = $allSlides[$tags[$tag][1]];
+                $output->addSlide($slide1);
+                _("Adding slide " . $slide1->getId());
+                $tags = $this->removeSlide($slide1->getId(), $tags);
+
+                $output->addSlide($slide2);
+                _("Adding slide " . $slide2->getId());
+                $tags = $this->removeSlide($slide2->getId(), $tags);
+
+                $nextTags = $slide2->getTags();
+                $nextTags = array_filter($nextTags, function ($next) use ($tag) {
+                    return $next !== $tag;
+                });
+                $nextTags = array_values($nextTags);
+                // TODO score Next Tag Stage
+                if (!empty($nextTags)) {
+                    $nextTag = $nextTags[0];
+                } else {
+                    $nextTag = nul;
+                }
+
+                while ($nextTag) {
+                    $slideIds = $tags[$nextTag];
+                    if (count($slideIds) >= 1) {
+                        $slideNext = $allSlides[$slideIds[0]];
+                        $output->addSlide($slideNext);
+                        _("Adding slide " . $slideNext->getId());
+                        $tags = $this->removeSlide($slideNext->getId(), $tags);
+
+                        $nextTags = $slideNext->getTags();
+                        $nextTags = array_filter($nextTags, function ($next) use ($nextTag) {
+                            return $next !== $nextTag;
+                        });
+                        $nextTags = array_values($nextTags);
+                        if (!empty($nextTags)) {
+                            $nextTag = $nextTags[0];
+                        } else {
+                            $nextTag = null;
+                        }
+                    } else {
+                        $nextTag = null;
+                    }
+                }
+            }
+        }
         echo $output;
     }
-    private function sliceRow(int $r1, int $r2, Input $input): Row
-    {
-        $c1 = 0;
-        $c2 = $c1 + 1 ;
-        $lastSlice = new Slice(0, 0, 0, 0, $input);
-        $row = new Row();
 
-        _("Slicing Row $r1, $r2");
-        while ($c2 <= $input->getC()) {
-            $slice = new Slice($r1, $c1, $r2, $c2, $input);
-            _($slice);
-            _($slice->getContents());
-            if ($slice->isValid($input->getL(), $input->getH())) {
-                _('valid');
-                if ($slice->getScore() < $lastSlice->getScore()) {
-                    _('adding slice');
-                    $row->addSlice($slice);
-                    $c1 = $c2 + 1;
-                    $c2 = $c1 + 1;
-                    $lastSlice = new Slice(0, 0, 0, 0, $input);
-                } else {
-                    $c2++;
-                    $lastSlice = $slice;
-                }
+    private function populateVerticals(array $verticals, int $slideId): array
+    {
+        $halfSlide = null;
+        $slides = [];
+        foreach ($verticals as $photo) {
+            if ($halfSlide === null) {
+                $halfSlide = new Slide($slideId);
+                $halfSlide->addPhoto($photo);
             } else {
-                _('invalid');
-                if (!$slice->isValidSize($input->getH())) {
-                    _('invalid size');
-                    if ($lastSlice->isValid($input->getL(), $input->getH())) {
-                        _('adding prev slice');
-                        $row->addSlice($lastSlice);
-                        $c1 = $c2 - 1;
-                        $c2 = $c1 + 1;
-                        $lastSlice = new Slice(0, 0, 0, 0, $input);
-                    } else {
-                        $c1 = $c1 + 1;
-                        $c2 = $c1 + 1;
-                    }
-                } else {
-                    _('invalid content');
-                    $c2++;
+                $halfSlide->addPhoto($photo);
+                $slides[$slideId] = $halfSlide;
+                $halfSlide = null;
+                $slideId++;
+            }
+        }
+        return $slides;
+    }
+
+    private function removeSlide(int $id, array $tags): array
+    {
+        foreach ($tags as $tag => $ids) {
+            $pos = array_search($id, $ids);
+            if ($pos !== false) {
+                _("Removing $id from $tag");
+                //print_r($tags);
+                unset($tags[$tag][$pos]);
+                //print_r($tags);
+                if (!empty($tags[$tag])) {
+                    $tags[$tag] = array_values($tags[$tag]);
                 }
             }
         }
-        return $row;
+        return $tags;
     }
 }
